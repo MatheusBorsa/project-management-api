5ff7f3cda6fd6c8773cb6df96a483a27f52f3885
MatheusBorsa <matheusalek485@icloud.com>
2025-09-19 00:07:10 -0300

    feat: rate limiting middleware

---
+++ app/Http/Middleware/RateLimitMiddleware.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use App\Utils\ApiResponseUtil;
+use Symfony\Component\HttpFoundation\Response;
+
+class RateLimitMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $key = "rate_limit:{$request->ip()}";
+        $limit = 60;
+        $decay = 60;
+
+        $requests = Cache::get($key, 0);
+
+        if ($request >= $limit) {
+            return ApiResponseUtil::error(
+                'Too many requests.',
+                null,
+                Response::HTTP_TOO_MANY_REQUESTS,
+            );
+        }
+
+        Cache::put($key, $requests + 1, $decay);
+
+        return $next($request);
+    }
+}
