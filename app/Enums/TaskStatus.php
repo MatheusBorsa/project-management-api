8b29902afff3b89497212b5cbf555df61002ab3b
MatheusBorsa <matheusalek485@icloud.com>
2025-09-22 15:22:45 -0300

    feat: updated task status

--- app/Enums/TaskStatus.php
+++ app/Enums/TaskStatus.php
@@ -6,7 +6,6 @@ enum TaskStatus: string
 {
     case PENDING = 'pending';
     case IN_PROGRESS = 'in_progress';
-    case WAITING_APPROVAL = 'waiting_approval';
-    case REJECTED = 'rejected';
-    case APPROVED = 'approved'; 
+    case UNDER_REVIEW = 'under_review';
+    case COMPLETED = 'completed';
 }
