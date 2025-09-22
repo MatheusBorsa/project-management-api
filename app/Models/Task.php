a4cc38627a93ee1536b929d0d495a4a3dfb16d92
MatheusBorsa <matheusalek485@icloud.com>
2025-09-22 15:46:10 -0300

    feat: task method for getting arts

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -31,4 +31,9 @@ class Task extends Model
     {
         return $this->hasMany(TaskHistory::class);
     }
+
+    public function arts()
+    {
+        return $this->hasMany(Art::class);
+    }
 }
